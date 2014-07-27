457a1f846fcb73b280494b5c5039e1ea695d3cbe
flosell <florian.sellmayr@gmail.com>
2014-07-27 21:33:37 +0200

    pulling bookmarks for tags from pinboard

--- background.js
+++ background.js
@@ -1,3 +1,31 @@
+var Pinboard = function(settings) {
+  var module = {};
+
+   var bookmarkUrl = function(tags,callback) {
+    
+    settings.getAuthToken(function(authToken) {
+      var tagQuery = "tag="+tags.join(",");
+      callback("https://api.pinboard.in/v1/posts/all?auth_token="+authToken+"&format=json&"+tagQuery);
+    });
+  }
+
+  module.getBookmarksForTags = function(tags,callback) {
+    bookmarkUrl(tags,function(url) {
+      $.getJSON(url,function(data) {
+        var bookmarks = data.map(function(entry) {
+          return {
+            url: entry.href,
+            title: entry.description
+          }
+        });
+
+        callback(bookmarks);
+      });
+    });
+  };
+  return module;
+};
+
 var Bookmarks = function() {
   var module = {};
   var BOOKMARKS_BAR_ID = "1";
@@ -57,5 +85,20 @@ var Bookmarks = function() {
 
   
 
+  return module;
+}
+
+var Settings = function() {
+  // can we find a more secure way of storing those credentials?
+  var module = {}
+  module.getAuthToken = function(callback) {
+    chrome.storage.local.get("auth_token",function(result) {
+      callback(result.auth_token)
+    });
+  }
+
+  module.setAuthToken = function(token) {
+    chrome.storage.local.set({'auth_token':token});
+  }
   return module;
 }
