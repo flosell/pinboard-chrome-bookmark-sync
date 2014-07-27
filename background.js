e679db7fea705318db0a0941f6130e8a5d8a60af
flosell <florian.sellmayr@gmail.com>
2014-07-27 22:09:33 +0200

    integration, fully functioning one-way sync; also removed functionality to create folders since at the moment we configure already created folders with marker-bookmarks in them

--- background.js
+++ background.js
@@ -1,3 +1,20 @@
+var App = function(pinboardModule, bookmarksModule) {
+  var module = {};
+
+  module.sync = function() {
+    bookmarksModule.findTargetFolders(function(targetFolders) {
+      targetFolders.forEach(function(targetFolder) {
+        pinboardModule.getBookmarksForTags(targetFolder.tags,function(bookmarks) {
+          bookmarksModule.setBookmarks(targetFolder.folderId,bookmarks);
+        });
+      });
+    });
+  };
+
+  return module;
+  
+}
+
 var Pinboard = function(settings) {
   var module = {};
 
@@ -28,33 +45,9 @@ var Pinboard = function(settings) {
 
 var Bookmarks = function() {
   var module = {};
-  var BOOKMARKS_BAR_ID = "1";
-  var createBookmarkFolder = function(folderName,callback) {
-    chrome.bookmarks.search({
-      title: folderName
-    },function(results) {
-      var existingFolders = results.filter(function(result) {
-        return result.parentId === BOOKMARKS_BAR_ID && result.url === undefined;
-      });
 
-      if (existingFolders.length === 0) {
-        chrome.bookmarks.create({'parentId': BOOKMARKS_BAR_ID,
-                               'title': folderName},
-                              function(newFolder) {
-          console.log("added folder: " + newFolder.title);
-          callback(newFolder);
-        });  
-      } else {
-        callback(existingFolders[0]);
-        console.log("Folder "+folderName+" already existed");
-      }
-      
-    });
-      
-  };
-
-  var createBookmark = function(parentNode, bookmark) {
-    var children = chrome.bookmarks.getChildren(parentNode.id,function(children) {
+  var createBookmark = function(parentId, bookmark) {
+    var children = chrome.bookmarks.getChildren(parentId,function(children) {
       var existingBookmarksWithSameUrl = children.filter(function(child) {
         return child.url.indexOf(bookmark.url)!=-1 && Math.abs(child.url.length - bookmark.url.length) <=1; // hacky way to check for equal but tolerating trailing / that chrome mitght put in
       });
@@ -63,7 +56,7 @@ var Bookmarks = function() {
         chrome.bookmarks.create({
             title: bookmark.title,
             url: bookmark.url, 
-            parentId: parentNode.id
+            parentId: parentId
         },function() {
           console.log("Added",bookmark);
         });
@@ -74,11 +67,9 @@ var Bookmarks = function() {
     
   }
 
-  module.setBookmarks = function(folderName, bookmarks) {
-    createBookmarkFolder(folderName,function(folderNode) {
-      bookmarks.forEach(function(bookmark) {
-        createBookmark(folderNode,bookmark);
-      })
+  module.setBookmarks = function(folderId, bookmarks) {
+    bookmarks.forEach(function(bookmark) {
+      createBookmark(folderId,bookmark);
     })
   };
 
